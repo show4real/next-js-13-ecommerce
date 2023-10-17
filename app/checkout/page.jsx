6c093c4381d615307f785de402e81fd40b870006
Showole Oladayo <show4ril@gmail.com>
2023-10-17 16:48:38 +0100

    changes

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,13 +1,5 @@
+"use client";
 import Checkout from "/app/components/Checkout";
-
-export const metadata = {
-  title: "Uk Used Laptops Mobile Phones at Affordable Prices",
-  openGraph: {
-    title: "UK used Gadgets and Appliances at Affordable prices",
-    description:
-      "Uk used Laptops Mobile Phone Samsung Iphone Dell HP Lenovo at affordable prices",
-  },
-};
 
 export default function OrderSummary() {
   return (
--- app/store/zustand.js
+++ app/store/zustand.js
@@ -1,39 +1,58 @@
 import { create } from "zustand";
+import Cookies from "js-cookie";
 
-const useCartStore = create((set) => ({
-  cart: [], // Initialize cart as an empty array initially
-  addToCart: (item) => {
-    set((state) => {
-      const updatedCart = [...state.cart, { ...item, quantity: 1 }];
-      return { cart: updatedCart };
-    });
-  },
-  removeFromCart: (itemId) => {
-    set((state) => {
-      const updatedCart = state.cart.filter(
+const useCartStore = create((set) => {
+  let storedCart = [];
+
+  if (typeof window !== "undefined") {
+    const cartFromCookie = Cookies.get("cart");
+    if (cartFromCookie) {
+      storedCart = JSON.parse(cartFromCookie);
+    }
+  }
+
+  return {
+    cart: storedCart,
+    addToCart: (item) => {
+      const updatedCart = [...storedCart, { ...item, quantity: 1 }];
+      if (typeof window !== "undefined") {
+        Cookies.set("cart", JSON.stringify(updatedCart), { expires: 7 });
+      }
+      set({ cart: updatedCart });
+    },
+    removeFromCart: (itemId) => {
+      const updatedCart = storedCart.filter(
         (cartItem) => cartItem.id !== itemId
       );
-      return { cart: updatedCart };
-    });
-  },
-  isItemInCart: (itemId) =>
-    state.cart.some((cartItem) => cartItem.id === itemId),
-  updateCart: (itemId, quantity) => {
-    set((state) => {
-      const updatedCart = state.cart.map((cartItem) =>
+      if (typeof window !== "undefined") {
+        Cookies.set("cart", JSON.stringify(updatedCart), { expires: 7 });
+      }
+      set({ cart: updatedCart });
+    },
+    isItemInCart: (itemId) =>
+      storedCart.some((cartItem) => cartItem.id === itemId),
+    updateCart: (itemId, quantity) => {
+      const updatedCart = storedCart.map((cartItem) =>
         cartItem.id === itemId ? { ...cartItem, quantity } : cartItem
       );
-      return { cart: updatedCart };
-    });
-  },
-  getTotalCartPrice: () => {
-    return state.cart.reduce((total, cartItem) => {
-      return total + cartItem.price * cartItem.quantity;
-    }, 0);
-  },
-  clearCart: () => {
-    return { cart: [] }; // Clear the cart state
-  },
-}));
+      if (typeof window !== "undefined") {
+        Cookies.set("cart", JSON.stringify(updatedCart), { expires: 7 });
+      }
+      set({ cart: updatedCart });
+    },
+    clearCart: () => {
+      if (typeof window !== "undefined") {
+        Cookies.remove("cart");
+      }
+      set({ cart: [] });
+    },
+    getTotalCartPrice: () => {
+      return storedCart.reduce(
+        (total, cartItem) => total + cartItem.price * cartItem.quantity,
+        0
+      );
+    },
+  };
+});
 
 export default useCartStore;
