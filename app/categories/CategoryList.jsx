2afc4fc547ca4ffb721668eec487e662161d8558
Showole Oladayo <show4ril@gmail.com>
2023-10-22 15:55:20 +0100

    changes

--- app/categories/CategoryList.jsx
+++ app/categories/CategoryList.jsx
@@ -15,14 +15,11 @@ export default function CategoryList({ section }) {
     fetchBrands();
   }, [search, rows]);
 
-  const fetchCategories = () => {
+  const fetchCategories = async () => {
     setLoading(true);
-    getAllCategories({ search, rows }).then(
+    await getAllCategories({ search, rows }).then(
       (res) => {
         setCategories(res.categories.data);
-        setTimeout(() => {
-          setLoading(false);
-        }, 3000);
       },
       (error) => {
         setLoading(false);
@@ -45,42 +42,30 @@ export default function CategoryList({ section }) {
     );
   };
   return (
-    <div className="bg-gray-100">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900 capitalize mt-5 md:mt-0 lg:mt-0 xl:mt-0">
-            {section}
-          </h2>
-          {section == "categories" && (
-            <div className="mt-0 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-              {console.log(allcategories)}
-              {allcategories.map((category, key) => (
-                <Link href={`/${category.slug}`}>
-                  <div
-                    key={key}
-                    className="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg  border-gray-100 bg-white shadow-md"
-                  >
-                    <div className="relative mx-3 mt-3 flex h-72 overflow-hidden rounded-sm">
-                      <Link href={`/${category.slug}`}>
-                        {" "}
-                        <img
-                          src={category.image_url}
-                          alt={category.name}
-                          className="peer absolute top-0 right-0 h-full w-full object-contain"
-                        />
-                      </Link>
-                    </div>
-                    <h3 className="mt-6 pb-5 text-sm text-gray-500 pl-4">
-                      <span className="absolute inset-0" />
-                      {category.name}
-                    </h3>
-                  </div>
-                </Link>
-              ))}
+    <>
+      {allcategories.map((category, key) => (
+        <div
+          key={key}
+          className="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg  border-gray-100 bg-white shadow-md mt-20"
+        >
+          <div className="relative mx-3 mt-3 flex h-72 overflow-hidden rounded-sm">
+            <Link href={`/categories/${category.slug}`}>
+              <img
+                className="peer absolute top-0 right-0 h-full w-full object-contain"
+                src={category.image_url}
+                alt="category image"
+              />
+            </Link>
+          </div>
+          <Link href={`/categories/${category.slug}`}>
+            <div className="mt-4 px-5 pb-2">
+              <h5 className="text-sm tracking-tight text-slate-900">
+                {category.name}
+              </h5>
             </div>
-          )}
+          </Link>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
