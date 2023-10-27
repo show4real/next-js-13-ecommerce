6c4e654e473e815034013a026113a28c3a3fea5e
Showole Oladayo <show4ril@gmail.com>
2023-10-27 20:02:27 +0100

    changes

--- app/components/SocialIconMenu.jsx
+++ app/components/SocialIconMenu.jsx
@@ -12,81 +12,47 @@ const SocialIconMenu = () => {
 
   return (
     <>
-      {/* <div className="flex justify-center pb-10 md:pb-0 lg:pb-0 md:justify-end lg:justify-end xl:justify-end">
-        <div
-          class="bg-blue-100 border-t w-64 border-b border-blue-500 text-blue-700 px-4 py-3"
-          role="alert"
-        >
-          <p class="font-bold">Informational message</p>
-          <p class="text-sm">Some additional text to explain said message.</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 ">
-        <div className="col-span-5 md:col-span-12 text-center">
-          <span className="social-icon">
-            <SocialIcon url="https://twitter.com/hayzeeonline?t=CXwigiNkx0AZymf_uUwVwg&s=09" />
-          </span>
-          <span className="social-icon">
-            <SocialIcon url="https://instagram.com/hayzee_computer_resources?utm_source=qr&igshid=OGIxMTE0OTdkZA==" />
-          </span>
-          <span className="social-icon">
-            <SocialIcon url="https://www.tiktok.com/@hayzeecomputerresources?_t=8fZAlidBCxq&_r=1" />
-          </span>
-         
-          <span className="social-icon">
-            <SocialIcon url="https://www.facebook.com/Hayzeeonline?mibextid=cejktS" />
-          </span>
-        </div>
-      </div> */}
-
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div class="justify-center text-center"></div>
-        <div class="justify-center text-center md:pt-0 lg:pt-0 pt-16">
-          {" "}
-          <span className="social-icon">
-            <SocialIcon url="https://twitter.com/hayzeeonline?t=CXwigiNkx0AZymf_uUwVwg&s=09" />
-          </span>
-          <span className="social-icon">
-            <SocialIcon url="https://instagram.com/hayzee_computer_resources?utm_source=qr&igshid=OGIxMTE0OTdkZA==" />
-          </span>
-          <span className="social-icon">
-            <SocialIcon url="https://www.tiktok.com/@hayzeecomputerresources?_t=8fZAlidBCxq&_r=1" />
-          </span>
-          <span className="social-icon">
-            <SocialIcon url="https://www.facebook.com/Hayzeeonline?mibextid=cejktS" />
-          </span>
-        </div>
-        <div class="justify-self-center md:justify-self-end lg:justify-self-end md:pt-0 lg:pt-0 pt-5">
-          {" "}
-          <div
-            class="bg-orange-100 border-t w-64 border-b border-orange-500 text-orange-700 px-4 py-3"
-            role="alert"
-          >
-            <p class="font-bold">Notice</p>
-            <p
-              className="text-sm"
-              style={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: showFullText ? "unset" : 2,
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              The Exchange Rate volatility has indeed Negatively impact the
-              fluctuation of stock market Price. Which in turn render all price
-              on our different Platform invalid. Please kindly ask for current
-              price before making a final pick. Also of Note is that, the price
-              giving at an instant in time is only valid for that time. THANK
-              YOU Signed Hayzee Computer Resources
-              <br />
-            </p>
-            <button onClick={toggleText} className="text-gray-600 text-sm">
-              {showFullText ? "Hide" : "Show More"}
-            </button>
+      <div class="container mx-auto">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div class="flex justify-center text-center"></div>
+          <div class="flex justify-center text-center md:pt-0 lg:pt-0 pt-3">
+            <div class="flex">
+              <span className="social-icon">
+                <SocialIcon url="https://twitter.com/hayzeeonline?t=CXwigiNkx0AZymf_uUwVwg&s=09" />
+              </span>
+              <span className="social-icon">
+                <SocialIcon url="https://instagram.com/hayzee_computer_resources?utm_source=qr&igshid=OGIxMTE0OTdkZA==" />
+              </span>
+              <span className="social-icon">
+                <SocialIcon url="https://www.tiktok.com/@hayzeecomputerresources?_t=8fZAlidBCxq&_r=1" />
+              </span>
+              <span className="social-icon">
+                <SocialIcon url="https://www.facebook.com/Hayzeeonline?mibextid=cejktS" />
+              </span>
+            </div>
+          </div>
+          <div class="flex justify-center md:justify-end lg:justify-end md:pt-0 lg:pt-0 pt-3">
+            <div class="bg-orange-100 border-t border-b border-orange-500 text-orange-700 w-full md:w-64 px-4 py-3">
+              <p class="font-bold">Notice</p>
+              <p
+                class="text-sm overflow-hidden"
+                style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;"
+              >
+                The Exchange Rate volatility has indeed Negatively impact the
+                fluctuation of stock market Price. Which in turn render all
+                price on our different Platform invalid. Please kindly ask for
+                current price before making a final pick. Also of Note is that,
+                the price giving at an instant in time is only valid for that
+                time. THANK YOU Signed Hayzee Computer Resources
+              </p>
+              <button onclick="toggleText()" class="text-gray-600 text-sm">
+                Show More
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="grid grid-cols-1">
         <CategorySlider sale_type={"flash sales"} />
       </div>
