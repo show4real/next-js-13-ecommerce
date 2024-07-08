79e2a36f0b4f150b850cc294768c513eaae666b0
Showole Oladayo <show4ril@gmail.com>
2024-07-08 21:08:57 +0100

    google site verification

--- app/page.jsx
+++ app/page.jsx
@@ -33,7 +33,10 @@ export const metadata = {
   },
 
   other: {
-    "google-site-verification": "4Zkz1j8swUUwJUJrRx2wsqs4YwJy6ru1Xb-9WmFnjek",
+    "google-site-verification": [
+      "4Zkz1j8swUUwJUJrRx2wsqs4YwJy6ru1Xb-9WmFnjek",
+      "bV_J8Ba7C_BjvpuElY6jzsTkavuCYVTwhgPpwRA-TMc",
+    ],
   },
   applicationName: "Ecommerce Website for Laptops and Mobile phone",
 };
