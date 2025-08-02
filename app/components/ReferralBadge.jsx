e20052525100cc15803d7263363322d1f75497ba
show4real <show4ril@gmail.com>
2025-08-02 11:28:27 +0100

    update

--- app/components/ReferralBadge.jsx
+++ app/components/ReferralBadge.jsx
@@ -58,14 +58,14 @@ const ReferralBadge = () => {
         return !value
           ? "Email is required"
           : validateEmail(value)
-          ? ""
-          : "Please enter a valid email address";
+            ? ""
+            : "Please enter a valid email address";
       case "phone":
         return !value
           ? "Phone is required"
           : validatePhone(value)
-          ? ""
-          : "Please enter a valid phone Number";
+            ? ""
+            : "Please enter a valid phone Number";
       default:
         return "";
     }
@@ -166,11 +166,25 @@ const ReferralBadge = () => {
       {showBadge && (
         <div className="fixed bottom-5 left-3 z-50 bg-blue-500 text-white px-6 py-3 rounded-lg flex items-center">
           <div className="mr-2">
-            <FontAwesomeIcon icon={faGift} />
+            {/* Telegram icon - you can use FontAwesome's faTelegram or a custom SVG */}
+            <svg
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
+            </svg>
           </div>
-          <button className="mr-2" onClick={handleOpenModal}>
-            Refer and Earn
-          </button>
+          <a
+            href="https://t.me/m/-uUMffD8OWJk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-2 hover:underline"
+            onClick={handleOpenModal}
+          >
+            Chat Telegram
+          </a>
           <div className="cursor-pointer" onClick={handleCloseBadge}>
             <FontAwesomeIcon icon={faTimes} />
           </div>
