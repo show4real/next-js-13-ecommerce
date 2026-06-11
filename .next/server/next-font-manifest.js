a8c78f3de766f872a0aaedb63ce11dfb29a42597
show4real <show4ril@gmail.com>
2026-06-11 13:52:30 +0100

    update

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/macbook/Desktop/hamzeal/hayzeecomputer/app/layout\":[\"static/media/0a86735c6520d94f-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
